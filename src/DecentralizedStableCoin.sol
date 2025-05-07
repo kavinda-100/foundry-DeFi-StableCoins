3b8cd22e7b9b23fd4d73934f2bafc2125c3b31c6
kavinda-100 <kavindarathnayake100@gmail.com>
2025-05-07 10:15:55 +0530

    feat: add Decentralized Stable Coin contract

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+/**
+ * @title Decentralized Stable Coin (DSC)
+ * @dev A decentralized stable coin contract that allows users to mint and redeem stable coins against collateral.
+ * @author kavinda rathnayake
+ * Collateral: Exogenous assets (e.g., ETH, BTC, etc.)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to a fiat currency (e.g., USD)
+ */
+contract DecentralizedStableCoin {
+    constructor() {}
+}
