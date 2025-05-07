6d3b5bddd61dbf44733e84e2534ee072ea0b223a
kavinda-100 <kavindarathnayake100@gmail.com>
2025-05-07 10:20:05 +0530

    feat: add OpenZeppelin contracts submodule and update remappings

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
+import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
 /**
  * @title Decentralized Stable Coin (DSC)
  * @dev A decentralized stable coin contract that allows users to mint and redeem stable coins against collateral.
