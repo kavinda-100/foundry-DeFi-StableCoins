ba4517425b0ef84c9d9c9b6d383d151a146001a3
kavinda-100 <kavindarathnayake100@gmail.com>
2025-05-23 11:46:36 +0530

    feat: add Ownable functionality to DecentralizedStableCoin contract and implement owner-only burn logic

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 import  {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 /**
  * @author Kavinda Rathnayake
  * @title Decentralized Stable Coin
@@ -13,8 +14,28 @@ import  {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensi
  * 
  * This contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stable coin system.
  */
-contract DecentralizeStableCoin is ERC20Burnable {
+contract DecentralizeStableCoin is ERC20Burnable, Ownable {
+
+    // Errors -----------------------------------------------------
+    error DecentralizeStableCoin__MustBeMoreThanZero();
+    error DecentralizeStableCoin__BurnAmountExceedsBalance();
 
     constructor() ERC20("DecentralizeStableCoin", "DSC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        // get the balance of the user
+        uint256 balance = balanceOf(msg.sender);
+        // check if the user trying to burn zero tokens
+        if( _amount <= 0) {
+            revert DecentralizeStableCoin__MustBeMoreThanZero();
+        }
+        // check if the user has enough balance to burn
+        if( balance < _amount) {
+            revert DecentralizeStableCoin__BurnAmountExceedsBalance();
+        }
+        // Only the owner can burn tokens
+        super.burn(_amount);
+        
+    }
     
 }
