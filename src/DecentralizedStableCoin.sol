c153b758db3105ccda901651b5957557351b2fd2
kavinda-100 <kavindarathnayake100@gmail.com>
2025-05-23 11:40:48 +0530

    feat: implement DecentralizedStableCoin contract with OpenZeppelin ERC20Burnable

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import  {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 /**
  * @author Kavinda Rathnayake
  * @title Decentralized Stable Coin
@@ -12,4 +13,8 @@ pragma solidity ^0.8.24;
  * 
  * This contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stable coin system.
  */
-contract DecentralizeStableCoin {}
+contract DecentralizeStableCoin is ERC20Burnable {
+
+    constructor() ERC20("DecentralizeStableCoin", "DSC") {}
+    
+}
