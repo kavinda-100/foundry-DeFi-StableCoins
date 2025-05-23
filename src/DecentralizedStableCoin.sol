95cf00b164daef9e6f420af597c67017743b41f2
kavinda-100 <kavindarathnayake100@gmail.com>
2025-05-23 11:19:07 +0530

    feat: add DecentralizedStableCoin contract implementation

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+/**
+ * @author Kavinda Rathnayake
+ * @title Decentralized Stable Coin
+ * @dev This contract represents a decentralized stable coin.
+ * 
+ * Collateral: Exogenous (ETH, BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to a fiat currency (e.g., USD)
+ * 
+ * This contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stable coin system.
+ */
+contract DecentralizeStableCoin {}
