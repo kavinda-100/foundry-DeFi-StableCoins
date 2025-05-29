85e7825b0071dff9959be9cb854167e1ad3763d6
kavinda-100 <kavindarathnayake100@gmail.com>
2025-05-29 11:29:45 +0530

    feat: add Handler and InvariantsTest contracts for fuzz testing

---
+++ test/fuzz/Handler.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+
+contract Handler is Test {
+    constructor() {}
+}
---
+++ test/fuzz/InvariantsTest.t.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+
+contract InvariantsTest is Test {}
